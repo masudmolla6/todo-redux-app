2d8eb1365c3963afe314e5eac5a08684d1e7b075
masudmolla6 <masudmolla2937@gmail.com>
2025-01-01 16:51:58 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl text-center">Todo App.</h1>
+      <Button>My Button</Button>
+    </div>
+  )
+}
+
+export default App
